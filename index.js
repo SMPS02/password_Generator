74186ce497699059ce0c57f6e737b6fe19a4c21b
SMPS02 <simonmiguel1829@gmail.com>
2025-07-28 22:19:44 +0200

    Solo falta el RD

--- index.js
+++ index.js
@@ -1,95 +1,256 @@
+console.log("Generador de contraseñas");
+
 //VARIABLES GLOBALES
-//-------------------------------------------------
 
-//Boton que genera las contraseñas
-const button = document.getElementById("button");
+const inputNumero = document.getElementById("inputNumber");
+let contraseña = document.getElementById("textHolder");
+const botonGenerador = document.getElementById("generador");
+const botonCopiar = document.getElementById("copiar");
 
-//Contenedor que contiene las contraseñas
-const ul = document.getElementById("ul")
-
-//Array para sacar los caracteres de los cuales se formará la constraseña
-const caracteres = [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9","0", "a", "A", "b", "B",
+//Arrays
+const alfabeto = [
+    "a", "A", "b", "B",
     "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", 
     "j", "J", "k", "K", "l", "L","m", "M", "n", "N", "o", "O", "p", "P", 
     "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", 
     "x", "X", "z", "Z" 
 ]
+const numeros = [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+]
+const caracteresESP = [
+    "@", "#", "&", "?", "¿", "!", "¡"
+]
 
-//Contraseñas extraidas del local Storage
-const contraseñasDelLocal = obtenerDelLocal();
+//CheckBoxes
+const checkBoxGroup = document.querySelectorAll(`input[name="opcion"]`);
+checkBoxGroup.forEach(cb =>{
+   cb.addEventListener("change", () =>{
+        const marcados =[...checkBoxGroup].filter(cb => cb.checked);
 
-//Arreglo que sirve para almacenar la contraseña
-let cajaDeContraseñas = contraseñasDelLocal || [] 
+        if(marcados.length === 0){
+            cb.checked = true;
+        }
+    })
+})
 
 
-//FUNCIONES
+//Funcion(es)
+function numeroRandom(numero){
 
-//Fucnion para guardar las constraseñas en el local
-function agregarAlLocal(){
-    localStorage.setItem("contraseña", JSON.stringify(cajaDeContraseñas));
+    switch(numero){
+        case 1:
+            const randomAlfabeto = Math.floor(Math.random()*alfabeto.length);
+            return randomAlfabeto;
+        break;
+        case 2:
+            const randomNumeros = Math.floor(Math.random() *numeros.length);
+            return randomNumeros; 
+        break;
+        case 3:
+            const randomESP = Math.floor(Math.random()*caracteresESP.length);
+            return randomESP;
+        break;
+        case 4:
+            let fusion_1 = []
+            for(let i=0;i<alfabeto.length;i++){
+                fusion_1 = [...fusion_1, alfabeto[i]];
+            }
+            for(let i=0;i<numeros.length;i++){
+                fusion_1 = [...fusion_1, numeros[i]];
+            }
+
+            const numeroAlfabeto = Math.floor(Math.random()*fusion_1.length); 
+            return numeroAlfabeto;
+        break;
+        case 5:
+            let fusion_2 = []
+            for(let i=0;i<alfabeto.length;i++){
+                fusion_2 = [...fusion_2, alfabeto[i]];
+            }
+            for(let i=0;i<caracteresESP.length;i++){
+                fusion_2 = [...fusion_2, caracteresESP[i]];
+            }
+        
+            const alfabetoESP = Math.floor(Math.random()*fusion_2.length);
+
+            return alfabetoESP;
+
+        break;
+        case 6:
+            let fusion_3 = []
+            for(let i=0;i<numeros.length;i++){
+                fusion_3 = [...fusion_3, numeros[i]];
+            }
+             for(let i=0;i<caracteresESP.length;i++){
+                fusion_3 = [...fusion_3, caracteresESP[i]];
+            }
+            const numeroESP = Math.floor(Math.random()*fusion_3.length);
+
+            return numeroESP;
+            
+        break;
+        case 7:
+            let fusion_4 = [];
+             for(let i=0;i<alfabeto.length;i++){
+                fusion_4 = [...fusion_4, alfabeto[i]];
+            }
+            for(let i=0;i<numeros.length;i++){
+                fusion_4 = [...fusion_4, numeros[i]];
+            }
+             for(let i=0;i<caracteresESP.length;i++){
+                fusion_4 = [...fusion_4, caracteresESP[i]];
+            }
+
+            const numAlfESP = Math.floor(Math.random()*fusion_4.length);
+            return numAlfESP;
+        break;
+        default:
+        return console.log("Este parametro es invalido");
+        break;
+    }
+    // const numero = Math.floor(Math.random() * caracteres.length);
+    // return numero
 }
 
-//Función para obetener las constraseñas del local
-function obtenerDelLocal(){
-    return JSON.parse(localStorage.getItem("contraseña"));
+function crearContraseña(){
+
+    let contraseñaGenerada = "";
+    
+    const  seleccionados = [...document.querySelectorAll(`input[name="opcion"]:checked`)]
+    .map(cb => cb.value)
+    .sort()
+    .join("+");
+
+    const acciones = {
+    'abc': () => {
+        for(let i = 0;i<inputNumero.valueAsNumber;i++){
+            contraseñaGenerada+=alfabeto[numeroRandom(1)];
+        }
+        contraseña.textContent = contraseñaGenerada;
+    },
+    '123': () => {
+        for(let i = 0;i<inputNumero.valueAsNumber;i++){
+            contraseñaGenerada+=numeros[numeroRandom(2)]
+        }
+        contraseña.textContent = contraseñaGenerada;
+    },
+    '@%&': () => {
+        for(let i = 0;i<inputNumero.valueAsNumber;i++){
+            contraseñaGenerada+=caracteresESP[numeroRandom(3)]
+        }
+
+        contraseña.textContent = contraseñaGenerada;
+    },
+    '123+abc': () => {
+         let fusion_1 = []
+            for(let i=0;i<alfabeto.length;i++){
+                fusion_1 = [...fusion_1, alfabeto[i]];
+            }
+            for(let i=0;i<numeros.length;i++){
+                fusion_1 = [...fusion_1, numeros[i]];
+            }
+
+            for(let i = 0;i<inputNumero.valueAsNumber;i++){
+                contraseñaGenerada+=fusion_1[numeroRandom(4)];
+            }
+            contraseña.textContent = contraseñaGenerada;
+    },
+    '@%&+abc': () => {
+       let fusion_2 = []
+            for(let i=0;i<alfabeto.length;i++){
+                fusion_2 = [...fusion_2, alfabeto[i]];
+            }
+            for(let i=0;i<caracteresESP.length;i++){
+                fusion_2 = [...fusion_2, caracteresESP[i]];
+            } 
+        //----------------------------------------------------------
+        //----------------------------------------------------------
+
+            for(let i = 0;i<inputNumero.valueAsNumber;i++){
+                contraseñaGenerada+=fusion_2[numeroRandom(5)]
+
+            }
+            contraseña.textContent = contraseñaGenerada
+        
+            
+    },
+    '123+@%&': () => {
+        let fusion_3 = []
+         for(let i=0;i<numeros.length;i++){
+                fusion_3 = [...fusion_3, numeros[i]];
+            }
+             for(let i=0;i<caracteresESP.length;i++){
+                fusion_3 = [...fusion_3, caracteresESP[i]];
+            }
+        //----------------------------------------------------------  
+        //----------------------------------------------------------  
+        for(let i = 0;i<inputNumero.valueAsNumber;i++){
+               
+            contraseñaGenerada+=fusion_3[numeroRandom(6)]
+            }
+            contraseña.textContent = contraseñaGenerada
+    },
+    '123+@%&+abc': () => {
+
+         let fusion_4 = [];
+             for(let i=0;i<alfabeto.length;i++){
+                fusion_4 = [...fusion_4, alfabeto[i]];
+            }
+            for(let i=0;i<numeros.length;i++){
+                fusion_4 = [...fusion_4, numeros[i]];
+            }
+             for(let i=0;i<caracteresESP.length;i++){
+                fusion_4 = [...fusion_4, caracteresESP[i]];
+            }
+        //----------------------------------------------------------
+        //----------------------------------------------------------
+        for(let i = 0;i<inputNumero.valueAsNumber;i++){
+                contraseñaGenerada+=fusion_4[numeroRandom(7)]
+        }
+        contraseña.textContent = contraseñaGenerada
+    },
+  };
+
+   if (acciones[seleccionados]) {
+    acciones[seleccionados]();  
+  } else {
+    console.log('Combinación no contemplada');
+  }
+
+
 }
 
-//Función para generar la contraseña en el HTML
-function subirAlHtml({texto, id}){
-    ul.innerHTML +=  
-    `
-     <li class="password" id= "${id}">
-        ${texto}
-     </li>
-     `
-}
+function copiarContraseña(){
 
-function numeroRandom(){
+    if(contraseña.value = ""){
+        alert("No hay nada para copiar")
+        return;
 
-    const numero = Math.floor(Math.random() * caracteres.length);
-    return numero
+    }else{
+        navigator.clipboard.writeText(contraseña.value).then(()=>{    
+        alert("Contraseña copiada")
+    }).catch((err)=>{
+        alert("Error en cargar la contraseña");
+    })
+    }
+   
 }
-
-function numeroRandomLimitado(){
-return Math.floor(Math.random() * (16-8 + 1) + 8)
-}
-//EVENTOS
+    
+    
+//Eventos
 
 document.addEventListener("click", (e)=>{
 
-    if(e.target.classList.contains("buttonGenerator")){
-
-        const contraseñaYaCreada = e.target.parentElement.nextElementSibling.firstElementChild.firstElementChild.firstElementChild;
-        localStorage.removeItem("contraseña");
-        cajaDeContraseñas = [];
-        ul.innerHTML = ""
+    //Crear la contraseña
+    if(e.target.classList.contains("generador") || e.target.classList.contains("play")){
+       crearContraseña();
         
-
-        //--------------------------------------------------------
-        //--------------------------------------------------------
-        let contraseñaGenerada = "";
-
-        for(let i = 0; i<numeroRandomLimitado();i++){
-           contraseñaGenerada += caracteres[numeroRandom()]
-        }
-
-        const contraseña = {
-            texto: contraseñaGenerada,
-            id: crypto.randomUUID()
-        }
-
-        cajaDeContraseñas = [...cajaDeContraseñas, contraseña];
-        subirAlHtml(contraseña);
-        agregarAlLocal();
-        console.log(contraseñaYaCreada);
-        }
     }
 
-    
-)
-window.addEventListener("DOMContentLoaded", ()=>{
-
-    cajaDeContraseñas.forEach(subirAlHtml);
-} )
-
+    //Copiar la contraseña
+    if(e.target.classList.contains("copiar")){
+        copiarContraseña();
+    }
+    console.log(e.target);
+})
